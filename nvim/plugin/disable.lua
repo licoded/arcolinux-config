15397ca9dc315ed8731892bc77f49585197c44e3
licoded <busy.li@foxmail.com>
2022-05-16 18:56:07 +0800

    small changes

--- nvim/plugin/disable.lua
+++ nvim/plugin/disable.lua
@@ -9,6 +9,6 @@ vim.keymap.set('n', '<Right>', '<NOP>')
 vim.keymap.set('n', '<Up>', '<NOP>')
 vim.keymap.set('n', '<Down>', '<NOP>')
 
--- Insert/Comman模式 删除键
-vim.keymap.set('i', '<Backspace>', '<NOP>')
-vim.keymap.set('c', '<Backspace>', '<NOP>') -- Ex/命令模式
+-- Insert/Comman模式 删除键(backspace)
+vim.keymap.set('i', '<BS>', '<NOP>')
+vim.keymap.set('c', '<BS>', '<NOP>') -- Ex/命令模式
--- nvim/plugin/markdown.lua
+++ nvim/plugin/markdown.lua
@@ -3,15 +3,22 @@ vim.api.nvim_create_autocmd('FileType', {
   callback = function()
     -- vim.keymap.set('i', ',f', '<Esc>/<++><CR>:nohlsearch<CR>c4l', { silent = true })
     -- vim.keymap.set('i', ',_', '<u>&ensp;**__**&ensp;</u><++><Esc>F_i', { silent = true })
-    vim.keymap.set('i', '-f', '<Esc>/<++><CR>:nohlsearch<CR>c4l', { silent = true })
-    vim.keymap.set('i', '--', '<u>&ensp;**__**&ensp;</u> <++><Esc>F_i', { silent = true })
-    vim.keymap.set('i', ',,', ', ', { silent = true })
-    vim.keymap.set('i', '..', '. ', { silent = true })
-    vim.keymap.set('i', '--a', '① ', { silent = true })
-    vim.keymap.set('i', '--b', '② ', { silent = true })
-    vim.keymap.set('i', '--c', '③ ', { silent = true })
-    vim.keymap.set('i', '--d', '④ ', { silent = true })
-    vim.keymap.set('i', '--e', '⑤ ', { silent = true })
+
+    -- 文章+选项
+    -- vim.keymap.set('i', '--', '<u>&ensp;**__**&ensp;</u> <++><Esc>F_i', { silent = true })
+    -- vim.keymap.set('i', '-f', '<Esc>/<++><CR>:nohlsearch<CR>c4l', { silent = true })
+    -- vim.keymap.set('i', ',,', ', ', { silent = true })
+    -- vim.keymap.set('i', '..', '. ', { silent = true })
+    -- vim.keymap.set('i', '--a', '① ', { silent = true })
+    -- vim.keymap.set('i', '--b', '② ', { silent = true })
+    -- vim.keymap.set('i', '--c', '③ ', { silent = true })
+    -- vim.keymap.set('i', '--d', '④ ', { silent = true })
+    -- vim.keymap.set('i', '--e', '⑤ ', { silent = true })
+
+    -- 生词记录
+    vim.keymap.set('i', '--', '<CR>- ', { silent = true })
+    vim.keymap.set('i', '-c', '……', { silent = true })
+    vim.keymap.set('i', '-e', '...', { silent = true })
   end
 })
 
